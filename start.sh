8dbcd408332bab1e29ec6fdea868d96bf2d7cdfd
RudraBJoshi <rudraj2022@gmail.com>
2026-05-13 12:54:40 -0700

    Start script

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Start frontend static server in the background
+python3 -m http.server 5500 --directory frontend &
+FRONTEND_PID=$!
+
 # Activate the virtual environment
 source venv/bin/activate
 
@@ -8,3 +12,6 @@ cd backend
 
 # Start the FastAPI server with uvicorn
 uvicorn app:app --reload
+
+# Kill frontend server when backend exits
+kill $FRONTEND_PID
